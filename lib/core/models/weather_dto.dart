8960f8d54079e21e713c81101774f367f98e8350
sstkachev <sstkachev@gmail.com>
2022-09-14 23:02:57 +0300

    put models

---
+++ lib/core/models/weather_dto.dart
@@ -0,0 +1,90 @@
+import 'package:free_api_weather_app/core/models/sys.dart';
+import 'package:free_api_weather_app/core/models/weather.dart';
+import 'package:free_api_weather_app/core/models/wind.dart';
+
+import 'clouds.dart';
+import 'coord.dart';
+import 'main.dart';
+
+class WeatherDto {
+  Coord? coord;
+  List<Weather>? weather;
+  String? base;
+  Main? main;
+  num? visibility;
+  Wind? wind;
+  Clouds? clouds;
+  num? dt;
+  Sys? sys;
+  num? timezone;
+  num? id;
+  String? name;
+  num? cod;
+
+  WeatherDto(
+      {this.coord,
+      this.weather,
+      this.base,
+      this.main,
+      this.visibility,
+      this.wind,
+      this.clouds,
+      this.dt,
+      this.sys,
+      this.timezone,
+      this.id,
+      this.name,
+      this.cod});
+
+  WeatherDto.fromJson(Map<String, dynamic> json) {
+    coord = json['coord'] != null ? new Coord.fromJson(json['coord']) : null;
+    if (json['weather'] != null) {
+      weather = <Weather>[];
+      json['weather'].forEach((v) {
+        weather!.add(new Weather.fromJson(v));
+      });
+    }
+    base = json['base'];
+    main = json['main'] != null ? new Main.fromJson(json['main']) : null;
+    visibility = json['visibility'];
+    wind = json['wind'] != null ? new Wind.fromJson(json['wind']) : null;
+    clouds =
+        json['clouds'] != null ? new Clouds.fromJson(json['clouds']) : null;
+    dt = json['dt'];
+    sys = json['sys'] != null ? new Sys.fromJson(json['sys']) : null;
+    timezone = json['timezone'];
+    id = json['id'];
+    name = json['name'];
+    cod = json['cod'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.coord != null) {
+      data['coord'] = this.coord!.toJson();
+    }
+    if (this.weather != null) {
+      data['weather'] = this.weather!.map((v) => v.toJson()).toList();
+    }
+    data['base'] = this.base;
+    if (this.main != null) {
+      data['main'] = this.main!.toJson();
+    }
+    data['visibility'] = this.visibility;
+    if (this.wind != null) {
+      data['wind'] = this.wind!.toJson();
+    }
+    if (this.clouds != null) {
+      data['clouds'] = this.clouds!.toJson();
+    }
+    data['dt'] = this.dt;
+    if (this.sys != null) {
+      data['sys'] = this.sys!.toJson();
+    }
+    data['timezone'] = this.timezone;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['cod'] = this.cod;
+    return data;
+  }
+}
